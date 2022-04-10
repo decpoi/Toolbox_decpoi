57d290ffb51cbe505146b626dcbcd0ed074f2d31
decpoi <103270541+decpoi@users.noreply.github.com>
2022-04-11 00:26:23 +0430

    The Toolbox vers1

--- Toolbox_decpoi.c
+++ Toolbox_decpoi.c
@@ -8,10 +8,8 @@ void percent(void);
 void gpa(void);
 void TimeInterval(void);
 void timer(void);
-void cornometter(void);
-void shomarande(void);
-void game(void);
-void af(void);
+void stopwatch(void);
+void counter(void);
 int CheckDate(char a[11]);
 int CheckDate2(int a,int b);
 int CheckTime(char a[8]);
@@ -71,15 +69,6 @@ void main()
     setTextColor(7);
     printf("Counter\n\n\n");
     setTextColor(11);
-    printf("\t\t\t7.");
-    setTextColor(7);
-    printf("Game\n\n\n");
-    setTextColor(11);
-    setTextColor(11);
-    printf("\t\t\t8.");
-    setTextColor(7);
-    printf("A(LOVE)F\n\n\n");
-    setTextColor(11);
     printf("\t\t\t0.");
     setTextColor(7);
     printf("Exit\n\n\n");
@@ -110,22 +99,12 @@ void main()
     else if(strcmp(answer,"5")==0)
     {
         system("cls");
-        cornometter();
+        stopwatch();
     }
     else if(strcmp(answer,"6")==0)
     {
         system("cls");
-        shomarande();
-    }
-    else if(strcmp(answer,"7")==0)
-    {
-        system("cls");
-        game();
-    }
-    else if(strcmp(answer,"8")==0)
-    {
-       system("cls");
-       af();
+        counter();
     }
     else if(strcmp(answer,"0")==0)
     {
@@ -665,7 +644,7 @@ void timer(void)
     }
 
 }
-void cornometter(void)
+void stopwatch(void)
 {
     int h =0 ,m = 0,s = 0;
     for(h=0 ; h<1000 ; h++)
@@ -683,8 +662,8 @@ void cornometter(void)
                                 setTextColor(7);
                                 printf(":");
                                 setTextColor(10);
-                                printf("%02d\n\n\n\n\n\t\t\t\t\t\t\tA(LOVE)F",s);
-                                printf("\n\n\n\n\t\t\t\t\t\t\t    baraye bargasht 0 ro vared kon : ");
+                                printf("%02d\n\n\n\n\n\t\t\t\t\t\t\tDecpoi",s);
+
 
 
                                 Sleep(1000);
@@ -699,7 +678,7 @@ void cornometter(void)
 
     }
 }
-void shomarande(void)
+void counter(void)
 {
     int counter = 0;
     char c;
@@ -708,11 +687,11 @@ void shomarande(void)
     {
 
         setTextColor(14);
-        printf("\n\n\t'y' ro bezan bara afzayesh , baraye bazgasht 0 : ");
+        printf("\n\n\tPress 'y' To count Or 0 to Return : ");
         setTextColor(11);
         printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\tcounter : %d\n\n\n\n\n\t\t\t\t\t\t\t",counter);
         setTextColor(10);
-        printf("A(LOVE)F");
+        printf("Decpoi");
         c = getch();
         if(c == 'y')
         {
@@ -725,498 +704,6 @@ void shomarande(void)
 
         system("cls");
     }
-}
-void game(void)
-{
-    setTextColor(7);
-    void clrscr();
-
-    int i,j;
-    char z;
-    int x;
-    char a[3][3];
-    clrscr();
-     // z baraie Y or N
-    //randomize();
-    x=rand()   ;
-    x= ((x)%2);
-
-        printf( "+------------------------------------------------------------------------------+\r");
-        printf( "| PROJECT NAME: DOOZ |\r");
-        printf( "+------------------------------------------------------------------------------+ \r\n");
-
-
-    a[0][0]='1'; a[0][1]='2';a[0][2]='3';
-    a[1][0]='4'; a[1][1]='5';a[1][2]='6';
-    a[2][0]='7'; a[2][1]='8';a[2][2]='9';
-    //
-    setTextColor(14);
-    printf("shoroo random : ");
-    if (x==0)
-     { printf ("Computer bayad shoroo kone...");   }
-    else
-     { printf ("to bayad shoroo koni...");      }
-
-    setTextColor(11);
-    printf ("\r\n\n to      :O \r\n computer :X \r\n");
-
-    setTextColor(7);
-    printf("\n 1 ro vared kon ta bazi shoroo beshe");
-    setTextColor(11);
-      getche();
-
-    while(x!=2) //     +++++++++++++++ halgheie koli +++++++++++++++++++++
-    {  //while
-
-    if(x==0)
-    {//if 1
-    x=1;
-
-    //win?
-    setTextColor(14);
-    for (i=0; i<3 ; i++)
-    {
-        if  (a[i][0]=='O' && a[i][1]=='O' && a[i][2]=='O')
-
-          {  printf("\n	to bordi !");
-             getche();                   }
-    }
-    //
-    setTextColor(14);
-    for (i=0; i<3 ; i++)
-    {
-       if  (a[0][i]=='O' && a[1][i]=='O' && a[2][i]=='O')
-
-          {  printf("\n	to bordi !");
-            getche();                        }
-    }
-    //
-    setTextColor(14);
-    if ( (a[0][0]=='O' && a[1][1]=='O' && a[2][2]=='O') || (a[0][2]=='O' && a[1][1]=='O' && a[2][0]=='O') )
-
-    {  printf("\n	to bordi !");
-      getche();                        }
-
-
-
-    //win 1 !!
-
-      else if (a[0][0]=='X' && a[0][1]=='X' && a[0][2]=='3')
-      a[0][2]='X';
-      else if (a[0][0]=='X' && a[0][2]=='X' && a[0][1]=='2')
-      a[0][1]='X';
-      else if (a[0][0]=='X' && a[1][0]=='X' && a[2][0]=='7')
-      a[2][0]='X';
-      else if (a[0][0]=='X' && a[2][0]=='X' && a[1][0]=='4')
-      a[1][0]='X';
-      else if (a[0][0]=='X' && a[1][1]=='X' && a[2][2]=='9')
-      a[2][2]='X';
-
-
-
-      else if (a[0][2]=='X' && a[0][1]=='X' && a[0][0]=='1')
-      a[0][0]='X';
-      else if (a[0][2]=='X' && a[1][1]=='X' && a[2][0]=='7')
-      a[2][0]='X';
-      else if (a[0][2]=='X' && a[1][2]=='X' && a[2][2]=='9')
-      a[2][2]='X';
-      else if (a[0][2]=='X' && a[2][2]=='X' && a[1][2]=='6')
-      a[1][2]='X';
-
-
-      else if (a[2][0]=='X' && a[1][0]=='X'&& a[0][0]=='1')
-      a[0][0]='X';
-      else if (a[2][0]=='X' && a[2][1]=='X' && a[2][2]=='9')
-      a[2][2]='X';
-      else if (a[2][0]=='X' && a[2][2]=='X' && a[2][1]=='8')
-      a[2][1]='X';
-      else if (a[2][0]=='X' && a[1][1]=='X' && a[0][2]=='3')
-      a[0][2]='X';
-
-
-      else if (a[2][2]=='X' &&  a[1][2]=='X'&& a[0][2]=='3')
-      a[0][2]='X';
-      else if (a[2][2]=='X' && a[2][1]=='X' && a[2][0]=='7')
-      a[2][0]='X';
-      else if (a[2][2]=='X' && a[1][1]=='X' && a[0][0]=='1')
-      a[0][0]='X';
-
-    //win 2 !!
-
-    else if (a[1][2]=='X' && a[1][1]=='X' && a[1][0]=='4')
-      a[1][0]='X';
-
-    else if (a[1][0]=='X' && a[1][1]=='X' && a[1][2]=='6')
-      a[1][2]='X';
-
-    else if (a[0][1]=='X' && a[1][1]=='X' && a[2][1]=='8')
-      a[2][1]='X';
-
-    else if (a[2][1]=='X' && a[1][1]=='X' && a[0][1]=='2')
-      a[0][1]='X';
-
-
-    //best
-    else if (a[1][1]=='5')
-    a[1][1]='X';
-
-    //anti lose !!
-    else if ( (a[0][1]=='O' && a[0][2]=='O' && a[0][0]=='1') ||(a[2][2]=='O' && a[1][1]=='O' && a[0][0]=='1')|| (a[1][0]=='O' && a[2][0]=='O' && a[0][0]=='1') )
-      a[0][0]='X';
-
-    else if ( (a[0][0]=='O' && a[0][2]=='O' && a[0][1]=='2') || (a[1][1]=='O' && a[2][1]=='O' && a[0][1]=='2') )
-      a[0][1]='X';
-
-    else if ( (a[1][2]=='O' && a[2][2]=='O' && a[0][2]=='3') || (a[2][0]=='O' && a[1][1]=='O' && a[0][2]=='3') || (a[0][0]=='O' && a[0][1]=='O' && a[0][2]=='3') )
-      a[0][2]='X';
-
-    else if ( (a[0][0]=='O' && a[2][0]=='O' && a[1][0]=='4') ||(a[1][2]=='O' && a[1][1]=='O' && a[1][0]=='4'))
-      a[1][0]='X';
-
-    else if ( (a[1][1]=='O' && a[1][0]=='O' && a[1][2]=='6') ||(a[0][2]=='O' && a[2][2]=='O' && a[1][2]=='6') )
-      a[1][2]='X';
-
-    else if ( (a[0][0]=='O' && a[1][0]=='O' && a[2][0]=='7') ||(a[0][2]=='O' && a[1][1]=='O' && a[2][0]=='7') || (a[2][2]=='O' && a[2][1]=='O' && a[2][0]=='7') )
-      a[2][0]='X';
-
-    else if ( (a[0][1]=='O' && a[1][1]=='O' && a[2][1]=='8') || (a[2][2]=='O' && a[2][0]=='O' && a[2][1]=='8') )
-      a[2][1]='X';
-
-    else if ( (a[0][0]=='O' && a[1][1]=='O' && a[2][2]=='9')||(a[0][2]=='O' && a[1][2]=='O' && a[2][2]=='9')||(a[0][0]=='O' && a[2][1]=='O' && a[2][2]=='9'))
-      a[2][2]='X';
-
-
-    //better 1
-    else if (a[2][2]=='9' && a[0][0]=='1')
-    a[2][2]='X';
-    else if (a[2][0]=='7' && a[0][2]=='3')
-    a[2][0]='X';
-
-    //better 2
-    else if (a[1][0]=='4' && a[1][2]=='6')
-    a[1][0]='X';
-    else if (a[0][1]=='2' && a[2][1]=='8')
-    a[0][1]='X';
-
-    //normal 1
-    else if (a[2][2]=='9')
-    a[2][2]='X';
-    else if (a[0][0]=='1')
-    a[0][0]='X';
-    else if (a[0][2]=='3')
-    a[0][2]='X';
-    else if (a[2][0]=='7')
-    a[2][0]='X';
-
-    //normal 2
-    else if (a[0][1]=='2')
-    a[0][1]='X';
-    else if (a[1][2]=='6')
-    a[1][2]='X';
-    else if (a[2][1]=='8')
-    a[2][1]='X';
-    else if (a[1][0]=='4')
-    a[1][0]='X';
-
-
-    else
-    {//else tt
-    printf("\n  mosavi shod !! \n ") ;
-    z='8';
-    }//else tt
-
-    //END GAME =====================================================================
-    while (z=='8')
-    {//bigin while
-    printf("\n dobare bazi konim?(Y/N)\r");
-    z=getche();
-
-    if (z=='y' || z=='Y')
-             {//==================
-              clrscr();
-              a[0][0]='1'; a[0][1]='2';a[0][2]='3';
-              a[1][0]='4'; a[1][1]='5';a[1][2]='6';
-              a[2][0]='7'; a[2][1]='8';a[2][2]='9';
-              setTextColor(14);
-              printf("ki shoroo konande bashe? \n 1.computer . \n 2.khodam \n 3.random") ;
-              z=getche();
-              if (z=='1')
-              x=0;
-              else if (z=='2')
-              x=1;
-              else if (z=='3')
-              {
-               //randomize ();
-               x=rand()   ;
-               x= ((x)%2);
-              }
-              else
-                 {
-                     setTextColor(7);
-                     printf ("\r\n\n '\a'  dobare emtehan kon (press 1 or 2 or 3) ! \r\n\n");   z='8';   }
-              } //=================
-
-    else if (z=='n' || z=='N')
-    main();
-
-    else
-                 {printf("\r\n\n \a dobare emtehan kon! ( press Y or N) \r\n\n");
-                 z='8';             }
-
-    }//end while
-    setTextColor(7);
-
-    //==============================================================================
-
-    }//if 1
-
-
-    //
-    clrscr();
-    printf ("\n\n") ;
-    printf ("        |        |          \r\n") ;; setTextColor(7); printf("    %c   |    %c   |   %c      \r\n",a[0][0],a[0][1],a[0][2]) ;
-    printf ("        |        |          \r\n") ;
-    printf ("  ______|________|______    \r\n") ;
-    setTextColor(14);
-    printf ("        |        |          \r\n") ;
-    printf ("        |        |          \r\n") ;
-    printf("    %c   |    %c   |   %c      \r\n",a[1][0],a[1][1],a[1][2]) ;
-    printf ("        |        |          \r\n") ;
-    printf ("  ______|________|______    \r\n") ;
-    setTextColor(11);
-    printf ("        |        |          \r\n") ;
-    printf ("        |        |          \r\n") ;
-    printf("    %c   |    %c   |   %c      \r\n",a[2][0],a[2][1],a[2][2]) ;
-    printf ("        |        |          \r\n") ;
-    //
-    setTextColor(7);
-    if (x==1)
-    { //if 2
-    x=0;
-
-
-    for (i=0; i<3 ; i++)
-    {
-    if  (a[i][0]=='X' && a[i][1]=='X' && a[i][2]=='X')
-
-    {
-
-    printf("\n  bakhti !");
-         z='8';                    }
-    }
-
-    for (i=0; i<3 ; i++)
-    {
-    if  (a[0][i]=='X' && a[1][i]=='X' && a[2][i]=='X')
-
-    {  printf("\n bakhti !");
-          z='8';                    }
-    }
-
-    if ( (a[0][0]=='X' && a[1][1]=='X' && a[2][2]=='X') || (a[0][2]=='X' && a[1][1]=='X' && a[2][0]=='X') )
-
-    {  printf("\n bakhti !");
-       z='8';                     }
-
-
-    if (z!='8')
-    {//start if vared kardan
-
-    char e;
-    e=getche();
-
-    //////  agar tamame khaneha zad e shode boodand
-    if  ( a[0][0]!='1' && a[0][1]!='2' && a[0][2]!='3' && a[1][0]!='4' && a[1][1]!='5' && a[1][2]!='6' && a[2][0]!='7' && a[2][1]!='8' && a[2][2]!='9')
-    { z='8';            }
-    //////-----------------------------------------
-
-
-    //vared kardane shomare
-    else if (e=='1' && a[0][0]!='X' && a[0][0]!='O' )
-    a[0][0]='O';
-
-    else if (e=='2' && a[0][1]!='X' && a[0][1]!='O' )
-    a[0][1]='O';
-
-    else if (e=='3' && a[0][2]!='X' && a[0][2]!='O' )
-    a[0][2]='O';
-
-    else if (e=='4' && a[1][0]!='X' && a[1][0]!='O' )
-    a[1][0]='O';
-
-    else if (e=='5' && a[1][1]!='X' && a[1][1]!='O' )
-    a[1][1]='O';
-
-    else if (e=='6' && a[1][2]!='X' && a[1][2]!='O' )
-    a[1][2]='O';
-
-    else if (e=='7' && a[2][0]!='X' && a[2][0]!='O' )
-    a[2][0]='O';
-
-    else if (e=='8' && a[2][1]!='X' && a[2][1]!='O' )
-    a[2][1]='O';
-
-    else if (e=='9' && a[2][2]!='X' && a[2][2]!='O' )
-    a[2][2]='O';
-
-    else
-    {  printf("\n  \a 2 bare talash kon");   x=1 ; }
-    }//end if vared kardan
-    }//if 2
-    }//while
-    return 0;
-    }
-
-    void clrscr(){
-    system("cls");
-    }
-
-void af(void)
-{
-    setTextColor(10);
-    printf("\n\n\n\n\t\t\tye adad vared kon : ");
-
-    int number;
-    setTextColor(11);
-    scanf("%d",&number);
-    setTextColor(7);
-    number++;
-
-    int num;
-
-    num = number;
-
-    int i , j , k;
-    printf("\n\n\n");
-
-            setTextColor(12);
-            //new
-            int size = number/2;
-
-            for (int x = 0; x < size; x++)
-            {
-                for (int y = 0; y <= 4*size; y++)
-                {
-                    double dist1 = sqrt(pow(x - size, 2) + pow(y - size, 2));
-                    double dist2 = sqrt(pow(x - size, 2) + pow(y - 3*size, 2));
-
-                    if (dist1 < size + 0.5 || dist2 < size + 0.5) {
-                        printf("*");
-                    }
-                    else {
-                        printf(" ");
-                    }
-                }
-                printf("\n");
-            }
-            for (int x = 1; x < 2*size; x++)
-            {
-                for (int y = 0; y < x; y++) {
-                    printf(" ");
-                }
-
-
-                for (int y = 0; y < 4*size + 1 - 2*x; y++) {
-                    printf("*");
-                }
-
-                printf("\n");
-            }
-
-               for(k=1;k<=number-1;k++)
-                {
-                    printf(" ");
-                    if(k==number-1 && number%2==0)
-                    {
-                        printf(" ");
-                    }
-                }
-
-
-            printf("*\n");
-    //new
-    for(i=1 ; i<=num ; i++)
-    {
-        setTextColor(14);
-        printf(" ");
-        if(i==num)
-        {
-            if(num == number)
-            {
-                printf("\b*");
-            }
-            else
-            {
-                printf("*");
-
-                    if(number - num - ((number/2)-1) != 0)
-                    {
-                      for(j=1 ; j<=2*(number - num)-1 ; j++)
-                        {
-                            printf(" ");
-                        }
-                    }
-                    else
-                    {
-                        for(j=1 ; j<=2*(number - num)-1 ; j++)
-                        {
-                            printf("*");
-                        }
-                    }
-
-
-
-                printf("*");
-            }
-
-            for(i=1 ; i<=num ; i++)
-            {
-                printf(" ");
-            }
-
-            setTextColor(11);
-            if(num == number)
-            {
-                for(k=1 ; k<=num ; k++)
-                {
-                    printf("*");
-                }
-            }
-            else if(number/2 + 1 == num)
-            {
-                for(k=1 ; k<=number/2 + 3 ; k++)
-                {
-                    printf("*");
-                }
-            }
-            else
-            {
-                printf("*");
-            }
-            setTextColor(7);
-
-            printf("\n");
-            num--;
-            i=1;
-        }
-
-    }
-    getchar();
-    setTextColor(7);
-
-    printf("\n\n\n\n\t\t\t\t\t\t\t    baraye bargasht 0 ro vared kon : ");
-
-
-
-    char exi[1];
-    setTextColor(11);
-    gets(exi);
-    setTextColor(7);
-    if(strcmp(exi,"0")==0)
-    {
-        main();
-    }
-
 }
 
 int CheckDate(char a[11])
